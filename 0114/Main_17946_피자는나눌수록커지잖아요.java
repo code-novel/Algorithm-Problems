a81d27b921eccabd0092a5d7700149e22f9058ee
JH <belarus92@naver.com>
2020-01-14 23:20:09 +0900

    0114 commit!!!

---
+++ 0114/Main_17946_피자는나눌수록커지잖아요.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main_17946_피자는나눌수록커지잖아요 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N=Integer.parseInt(br.readLine());
+		for(int i=0; i<N;++i) {
+			int K=Integer.parseInt(br.readLine());
+			System.out.println(1);
+		}
+	}
+}
