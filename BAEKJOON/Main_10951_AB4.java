3058679d398f1ff996958ac3a11cb5d1813e54db
code-novel <belarus92@naver.com>
2020-07-07 14:19:26 +0900

    0707 commit

---
+++ BAEKJOON/Main_10951_AB4.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main_10951_AB4 {
+	public static void main(String[] args) throws IOException {
+		 Scanner scan = new Scanner(System.in);
+	        while(scan.hasNextInt())
+	            System.out.println(scan.nextInt() + scan.nextInt());
+	}
+}
